f98d99d35099e5d1179d72a3a7ebb7e296626413
Stephen Van Dahm <code@vandahm.com>
2014-02-19 21:43:19 -0600

    Initial Project Creation.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Samantha's Blog Tutorial
+
+My friend is learning Ruby on Rails. I'm helping her.
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Samantha::Application.config.secret_key_base = 'f27e88c9773e25167657e15461c2953401531d7822be2aaff88d371908d8b1fa43d7c16492e91650be4eb89325bebda9c81f43c7a4191700ad86f7ef913e16ce'
